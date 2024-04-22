afb2ed0eeaea806da0e745375641a985fa6b10d7
unknown <sakshipadamwar2002@gmail.com>
2024-04-22 09:47:54 +0530

    added pie chart and garphs

--- src/componets/Charts/index.js
+++ src/componets/Charts/index.js
@@ -2,27 +2,45 @@ import React from "react";
 import { Line , Pie} from '@ant-design/charts';
 function ChartComponent({ sortedTransactions }) {
     console.log(sortedTransactions);
-    const data = sortedTransactions.map((item) => {
+    let data = sortedTransactions.map((item) => {
         return { date: item.date, amount: item.amount };
     });
 
-    let finalSpending = sortedTransactions.reduce((acc,obj)=>{
-       let key = obj.tag;
-       if(!acc[key]){
-        acc[key]={tag:obj.tag,amount:obj.amount};
-       }else{
-        acc[key].amount += obj.amount;
-       }
-       return acc;
-    },{})
+    
+     let spendingData = sortedTransactions.filter((transaction) =>{
+        if(transaction.type == "expense"){
+            return {tag:transaction.tag , amount:transaction.amount};
+        }
+     })
+
+     let finalSpending = spendingData.reduce((acc, obj) => {
+        let existingTag = acc.find((item) => item.tag === obj.tag);
+        if (existingTag) {
+            existingTag.amount += obj.amount;
+        } else {
+            acc.push({ tag: obj.tag, amount: obj.amount });
+        }
+        return acc;
+    }, []);
+    // let finalSpending = spendingData.reduce((acc,obj)=>{
+    //    let key = obj.tag;
+    //    if(!acc[key]){
+    //     acc[key]={tag:obj.tag,amount:obj.amount};
+    //    }else{
+    //     acc[key].amount += obj.amount;
+    //    }
+    //    return acc;
+    // },{})
     const config = {
         data:data,
+        width:500,
         autoFit: false,
         xField: 'date',
         yField: 'amount',
     };
     const spendingConfig = {
-        data:finalSpending,  
+        data:finalSpending,
+        width:500,  
         angleField:"amount",
         colorField:"tag"
     };
